916f21f440eb1fbdf2bb0dd3b3350f8b49c89e8e
GranKaen <63749175+GranKaen@users.noreply.github.com>
2020-11-10 01:31:11 -0800

    Add files via upload

---
+++ Udemy I am Poor/Udemy I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udemy I am Poor
+//
+//  Created by konstantine gudushauri on 11/10/20.
+//  Copyright © 2020 konstantine gudushauri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
